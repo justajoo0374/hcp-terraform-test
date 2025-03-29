35a3e37f711d88d7e46acd87db857f8cb6d65825
justajoo0374 <justajoo0374@gmail.com>
2025-03-29 15:28:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "hcp-vm" {
+  name         = "vm-without-providervlock"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  project = "magnetic-flare-454316-h0"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
